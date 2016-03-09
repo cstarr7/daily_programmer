a36f711f65d2f74816fefc33e56fc68fcd881790
Charles Starr <charles.starr7@gmail.com>
2016-03-09 00:17:42 -0600

    Currently empty, didn't feel like creating tonight

---
+++ intermediate_2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Author: Charles Starr
+# @Date:   2016-03-08 23:47:39
+# @Last Modified by:   Charles Starr
+# @Last Modified time: 2016-03-08 23:48:25
+#create a short text adventure that will call the user by their name. 
+#The text adventure should use standard text adventure commands 
+#("l, n, s, e, i, etc.").
+
+#I'm going to pass on this one for now. 
